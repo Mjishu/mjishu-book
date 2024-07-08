fc013694392fec39c67fd5d6a9ffd19b439d605a
Mjishu <128941980+Mjishu@users.noreply.github.com.>
2024-07-08 18:23:09 -0400

    styled post create

--- client/src/components/postComponents/createPost.jsx
+++ client/src/components/postComponents/createPost.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import Navbar from "../generalComponents/Navbar"
-import style from "../../styling/postStyles/postCreate.module.css"
-import {useNavigate} from "react-router-dom"
+import Navbar from "../generalComponents/Navbar";
+import style from "../../styling/postStyles/postCreate.module.css";
+import {useNavigate} from "react-router-dom";
+import {useUser} from "../userComponents/UserContext.jsx"
 
 function CreatePost(){
     const [postData,setPostData] = React.useState({
         message:"",
         image: "", //Not yet
     });
-    const [currentUser,setCurrentUser] = React.useState();
     const navigate = useNavigate()
-
-    React.useEffect(()=>{
-        fetch("/api/user/current")
-        .then(res=>res.json()).then(data => setCurrentUser(data))
-        .catch(err => console.error(`error fetching user ${err}`))
-    },[])
+    const fileInputRef = React.useRef(null)
+    const {currentUser} = useUser();
 
     function handleChange(e){
         const {name,value} = e.target;
@@ -36,15 +32,29 @@ function CreatePost(){
         .then(res=>res.json()).then(data=> data.message ==="success" && navigate("/"))
         .catch(err => console.error(err))
     }
+    function handleButtonClick(){
+        fileInputRef.current.click();
+    }
 
     return(
         <div className="content">
         <Navbar/>
+        <div className={style.body}>
         <form autoComplete="off" onSubmit={handleSubmit} className={style.form}>
+        <h2>Create Post</h2>
+        <div className={style.formInputs}>
             <label htmlFor="message">Message</label>
             <textarea type="text" name="message" onChange={handleChange} value={postData.message} className={style.messageInput}/>
-            <button className={style.postButton}>Submit</button>
+        <div className={style.buttons}>
+            <div className={style.imageInputHolder} onClick={handleButtonClick}>
+            <label htmlFor="image" className={style.imageLabel}><img src="/icons/upload.svg"/>Choose a file</label>
+            <input ref={fileInputRef}type="file" name="image" onChange={handleChange} value={postData.image} className={style.imageInput}/>
+        </div>
+        <button className={style.postButton}>Submit</button>
+        </div>
+        </div>
         </form>
+        </div>
         </div>
     )
 }
