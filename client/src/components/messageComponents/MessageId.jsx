358c75124ea500723b1e2fea684fd6051388b941
mjishu <128941980+Mjishu@users.noreply.github.com>
2024-08-28 22:45:28 -0400

    updated websocket urls

--- client/src/components/messageComponents/MessageId.jsx
+++ client/src/components/messageComponents/MessageId.jsx
@@ -1,67 +1,69 @@
 import React from "react";
 import MessageBody from "./MessageBody";
 import Navbar from "../generalComponents/Navbar";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import UserMessages from "./UserMessages";
 import style from "../../styling/messageStyles/messagedisplay.module.css";
 import useWebSocket from "react-use-websocket";
-import {useUser} from "../userComponents/UserContext.jsx";
+import { useUser } from "../userComponents/UserContext.jsx";
 
 //for prod replace WS_URL
-function MessageId(){
-    const [messageData,setMessageData] = React.useState();
-    const [loading,setLoading] = React.useState(true);
-    const [deleteMessage,setDeleteMessage] = React.useState(false);
+function MessageId() {
+    const [messageData, setMessageData] = React.useState();
+    const [loading, setLoading] = React.useState(true);
+    const [deleteMessage, setDeleteMessage] = React.useState(false);
     const navigate = useNavigate();
-    const {currentUser,isLoading} = useUser();
+    const { currentUser, isLoading } = useUser();
 
     const id = window.location.href.split("/")[window.location.href.split("/").length - 1]
 
     const WS_URL = "ws://mjishu-book-api.onrender.com/api/message/current-updates"
-    const {sendJsonMessage,lastJsonMessage,readyState} = useWebSocket(WS_URL,{
-        queryParams:{username:currentUser?.username, messageid:messageData?._id},share:true,
+    //const WS_URL = "ws://localhost:3000/api/message/current-updates"
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(WS_URL, {
+        queryParams: { username: currentUser?.username, messageid: messageData?._id }, share: true,
         shouldReconnect: (closeEvent) => true,
     });
 
-    React.useEffect(()=>{
-        if (readyState === WebSocket.OPEN && id){
-            sendJsonMessage({messageid:id});
+    React.useEffect(() => {
+        if (readyState === WebSocket.OPEN && id) {
+            sendJsonMessage({ messageid: id });
         }
-    },[readyState,id,sendJsonMessage]);
+    }, [readyState, id, sendJsonMessage]);
 
     React.useEffect(() => {
-        if(lastJsonMessage && lastJsonMessage.messageData._id === id){
+        if (lastJsonMessage && lastJsonMessage.messageData._id === id) {
             setMessageData(lastJsonMessage.messageData);
             setLoading(false);
         }
-    },[lastJsonMessage]);
+    }, [lastJsonMessage]);
 
-    if(loading){return <p>Loading...</p>}
+    if (loading) { return <p>Loading...</p> }
 
-    function handleDeleteSubmit(e){
+    function handleDeleteSubmit(e) {
         e.preventDefault()
 
-        fetch(`/api/message/find/${id}/delete`,{method:"DELETE", headers:{"Content-Type":"application/json"}})
-        .then(res => res.json())
-        .then(data => data.message === "success" ? navigate("/messages") : console.log(data))
-        .catch(error => console.error(`there was an error trying to delete message: $(error)`))
+        fetch(`/api/message/find/${id}/delete`, { method: "DELETE", headers: { "Content-Type": "application/json" } })
+            .then(res => res.json())
+            .then(data => data.message === "success" ? navigate("/messages") : console.log(data))
+            .catch(error => console.error(`there was an error trying to delete message: $(error)`))
 
         setDeleteMessage(false)
     }
 
     const deleteMessagePopup = (
         <div className="dialogBackdrop">
-        <form onSubmit={handleDeleteSubmit} className="editBoard">
-            <p>Are you sure you want to delete?</p>
-            <button onClick={() => setDeleteMessage(false)}>Cancel</button>
-            <button>Delete</button>
-        </form>
+            <form onSubmit={handleDeleteSubmit} className="editBoard">
+                <p>Are you sure you want to delete?</p>
+                <button onClick={() => setDeleteMessage(false)}>Cancel</button>
+                <button>Delete</button>
+            </form>
         </div>
     )
 
-    return(
+    return (
         <div>
-        <MessageBody body={messageData.body} id={messageData._id}/>
+            <MessageBody body={messageData.body} id={messageData._id} />
             <button onClick={() => setDeleteMessage(true)} className={style.deleteButton}>Delete</button>
             {deleteMessage && deleteMessagePopup}
         </div>
